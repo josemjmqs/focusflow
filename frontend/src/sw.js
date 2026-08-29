436f25ab3b8f147f10ad66e5d112c0efcc91eac5
Jose Diaz <joseignaciodiazh@gmail.com>
2026-08-29 18:34:32 -0400

    fix: actualizar service worker

--- frontend/src/sw.js
+++ frontend/src/sw.js
@@ -2,6 +2,14 @@ import { precacheAndRoute } from "workbox-precaching";
 
 precacheAndRoute(self.__WB_MANIFEST);
 
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
 self.addEventListener("push", (event) => {
   let datos = {
     titulo: "FocusFlow",
